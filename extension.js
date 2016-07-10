23fab4ca65ea3022f25bfdf5582ea8461cd78841
james-johnston <jaj@darkblack.org>
2016-07-10 16:03:48 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -5,7 +5,9 @@ new (function() {
       [' ', 'ONE: digital pin %m.pin setting %m.dsetting', 'setDigital', '1', 'off'],
       [' ', 'ONE: pwm pin %m.ppin setting %n', 'setPwm', '1', '100'],
       [' ', 'ONE: digital pin %m.pin get', 'getDigital', '1'],
-      [' ', 'ONE: pwm pin %m.ppin get', 'getPwm', '1']
+      [' ', 'ONE: pwm pin %m.ppin get', 'getPwm', '1'],
+      [' ', 'set ip address', 'setIP', '192.168.2.102'],
+      ['r', 'get ip address', 'getIP']
     ],
     'menus': {
       'pin': ['1', '2', '3'],
@@ -14,10 +16,17 @@ new (function() {
      },
     url: 'https://github.com/savaka2/scratch-extensions/wiki/Link-Opener-extension'
   };
+  ext.ip = '';
   ext._shutdown = function() {};
   ext._getStatus = function() {
     return {status: 2, msg: 'Device connected'};
   };
+  ext.setIP = function(newIP) {
+    ext.ip = newIP;
+  }
+  ext.getIP = function() {
+    return ext.ip;
+  }
   ext.getPwm = function(pin) {
   };
   ext.setPwm = function(pin, setting) {
